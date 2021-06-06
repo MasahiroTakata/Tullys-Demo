49bd0ec0001fc674a2e9cd7d90130520f17043db
Masahiro Takata <takata005581016@gmail.com>
2021-06-06 18:58:31 +0900

    main-visual-javascript2
    javascriptのレスポンシブ対応及び、配置の微調整の実装が完了

--- js/indexImage.js
+++ js/indexImage.js
@@ -2,7 +2,13 @@ window.onload = function (){
   var timer1 = null;
 
   function chengeImage(){
-    let image = document.getElementById('targetImage');
+    let image;
+
+    if(document.body.clientWidth > 768){
+      image = document.getElementById('targetImage_pc'); //pc版
+    } else{
+      image = document.getElementById('targetImage_sp'); //スマホ版
+    }
 
     if(image.style.opacity == ''){
       image.style.opacity = 1; // cssでopacityを設定していない場合に設定する。
@@ -16,11 +22,26 @@ window.onload = function (){
 
       if(image.style.opacity == 0){ // 透明度が0になった場合
         clearInterval(intervalId);
-        //画像を交換
-        if(image.src === "file:///Users/takatamasahiro/tully's_demo/Tullys-Demo/images/ae26e88550b587545778b941fd256d371df62a5d.jpg"){
-          image.src = "file:///Users/takatamasahiro/tully's_demo/Tullys-Demo/images/PC_MV_NewGoods210506PC_MV_NewGoods210506.jpg";
-        }else{
-          image.src = "file:///Users/takatamasahiro/tully's_demo/Tullys-Demo/images/ae26e88550b587545778b941fd256d371df62a5d.jpg";
+        let imgDirectory = "file:///Users/takatamasahiro/tully's_demo/Tullys-Demo/images/";
+
+        if(document.body.clientWidth > 768){
+          //画像を交換
+          if(image.src === imgDirectory + "ae26e88550b587545778b941fd256d371df62a5d.jpg"){
+            image.src = imgDirectory + "PC_MV_NewGoods210506PC_MV_NewGoods210506.jpg";
+          } else if(image.src === imgDirectory + "PC_MV_NewGoods210506PC_MV_NewGoods210506.jpg"){
+            image.src = imgDirectory + "PC_MV_2104_end.jpg";
+          } else if(image.src === imgDirectory + "PC_MV_2104_end.jpg"){
+            image.src = imgDirectory + "ae26e88550b587545778b941fd256d371df62a5d.jpg";
+          }
+        } else{
+          // スマホ版
+          if(image.src === imgDirectory + "MV_SP_210304.jpg"){
+            image.src = imgDirectory + "SP_MV_NewGoods210506SP_MV_NewGoods210506.jpg";
+          } else if(image.src === imgDirectory + "SP_MV_NewGoods210506SP_MV_NewGoods210506.jpg"){
+            image.src = imgDirectory + "SP_MV_2104_end.jpg";
+          } else if(image.src === imgDirectory + "SP_MV_2104_end.jpg"){
+            image.src = imgDirectory + "MV_SP_210304.jpg";
+          }
         }
         // フェードイン処理
         opacityInt = image.style.opacity * 100;
@@ -37,5 +58,5 @@ window.onload = function (){
     }, 100); // 100ミリ秒ごとに実行する
   }
 
-  timer1 = setInterval(chengeImage, 6000);
+  timer1 = setInterval(chengeImage, 6000); // 6秒ごとにchangeImage()呼び出し
 };
